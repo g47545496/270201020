608f7ebaa3ad3973d4aa0f02658548f761da63ec
Osman Altunay <73104422+g47545496@users.noreply.github.com>
2020-10-19 11:14:46 +0000

    Replys

---
+++ lab1/example2.py
@@ -0,0 +1,36 @@
+animls = 10
+chicken = 6
+roosters = animls - chicken
+print(roosters)
+#math expressions
+x=1
+y=4
+z=0.25
+results = ((2*x + y)**2 *z**0.5) / (x**0.5 + y**0.5)
+print(results)
+#solve equation
+a=2
+b=6
+c=-20
+x = (-b+(b**2-4*a*c)**0.5) / (2*a)
+
+print(x)
+#Temperature
+C = 32
+F = C*1.8+32
+print(F)
+#hypotenus
+x = 6
+y = 8
+z = (x**2+y**2)**0.5
+print(z)
+#another example
+a = 80
+b= 70
+between_lenght = 490
+the_remaining_way = 150
+
+leng = between_lenght-the_remaining_way
+t = (leng / (a+b))*60
+print(t)
+
